cc89c915cb84b468e29cc2592035c906fee1421c
gurpindersidhu178 <81562559+gurpindersidhu178@users.noreply.github.com>
2025-01-26 17:31:42 +0530

    Fixed JwtStrategy and updated AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    ConfigModule.forRoot(), // Ensures environment variables are loaded
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
+
