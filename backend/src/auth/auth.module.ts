231559d6a4b0a516586c71a400ddee56f7b18792
gurpindersidhu178 <81562559+gurpindersidhu178@users.noreply.github.com>
2025-01-27 12:30:26 +0530

    Fixed duplicate imports in auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { ConfigModule } from '@nestjs/config';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
